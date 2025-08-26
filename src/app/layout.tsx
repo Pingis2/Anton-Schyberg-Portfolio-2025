5b62ac366209d0480aead72719efc7a914146086
Pingis2 <anton@worksit.se>
2025-08-26 15:43:01 +0200

    some updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
+import Link from "next/link";
+import styles from "./page.module.scss";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +27,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <header className={styles.header}>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/career">Career</Link>
+            <Link href="/about">About Me</Link>
+            <Link href="/contact">Contact</Link>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
